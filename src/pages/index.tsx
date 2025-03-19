3337094bce3e5b800e8857989fdaf861d5aec153
Ming Ng <n.gihming@yahoo.com>
2025-03-19 22:05:45 +0800

    fix: added analytics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,11 @@ import Head from "next/head";
 // import ClickSpark from "@/components/bits/ClickSpark/ClickSpark";
 import WindowBlind from "@/components/custom/window-blind";
 // import Stairs from "@/components/custom/stair";
-
+import { Analytics } from "@vercel/analytics/react";
 export default function Home() {
   return (
     <>
+      <Analytics />
       <Head>
         <title>Diolehroma - A curated collection of diorama</title>
         <meta
