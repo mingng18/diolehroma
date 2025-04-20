5bccac0df05093b415088edcae085d6b6a4b3414
Ming Ng <n.gihming@yahoo.com>
2025-04-20 21:29:23 +0800

    remove react compiler

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    reactCompiler: true,
-  },
   reactStrictMode: true,
   webpack: (config) => {
     config.module.rules.push({
